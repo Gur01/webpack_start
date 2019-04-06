a3350ab93ea3ad883822884b1d1d623e3ff191c6
viacheslav <gv@betwinneraffiliates.com>
2019-04-06 19:17:30 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import './scss/main.scss';
 
-// import { hello } from './js/index.js';
-// import { goodbye } from './js/index.js';
-import greeting from '@/src/index.js'
+// // import { hello } from './js/index.js';
+// // import { goodbye } from './js/index.js';
+// import greeting from '@/src/index.js'
 
-// hello('hello')
-// goodbye('goodbye')
-greeting.goodbye('goodbye');
-// console.log(greeting.concat);
-const name = { name: 'john' }
-const age = { age: 25 }
-const concat = { ...name, ...age };
-console.log(concat);
+// // hello('hello')
+// // goodbye('goodbye')
+// greeting.goodbye('goodbye');
+// // console.log(greeting.concat);
+// const name = { name: 'john' }
+// const age = { age: 25 }
+// const concat = { ...name, ...age };
+// console.log(concat);
 
